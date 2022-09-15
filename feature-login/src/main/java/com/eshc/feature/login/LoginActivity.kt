58d8bc328a90cdd6c3ccebe2a810b1162f1d216a
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-15 20:19:52 +0900

    [FEATURE] #3 Login 화면 - GetAccessToken 기능 연동

--- feature-login/src/main/java/com/eshc/feature/login/LoginActivity.kt
+++ feature-login/src/main/java/com/eshc/feature/login/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.eshc.feature.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
@@ -14,6 +16,8 @@ class LoginActivity : AppCompatActivity() {
     private var _binding : ActivityLoginBinding? = null
     private val binding get() = _binding
 
+    private val viewModel : LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(
@@ -24,6 +28,14 @@ class LoginActivity : AppCompatActivity() {
 
         binding?.btLogin?.setOnClickListener {
             moveToGithubWeb()
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.data?.let { data ->
+            val code = data.getQueryParameter(getString(R.string.param_code))
+            viewModel.getAccessToken(code ?: "")
         }
     }
 

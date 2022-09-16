e982880f08bc2d5a66a761c445d544c8b921cf8c
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-16 20:38:16 +0900

    [FEATURE] #1 issue, notification, repo, user Repository Interface 수정

--- domain/src/main/java/com/eshc/domain/repository/RepoRepository.kt
+++ domain/src/main/java/com/eshc/domain/repository/RepoRepository.kt
@@ -1,4 +1,8 @@
 package com.eshc.domain.repository
 
+import com.eshc.domain.model.Repo
+import io.reactivex.rxjava3.core.Single
+
 interface RepoRepository {
+    fun getRepos() : Single<Result<List<Repo>>>
 }

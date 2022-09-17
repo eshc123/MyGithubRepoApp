4da7922f680988bc9cfbe3de35267c29501caedf
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-17 16:07:09 +0900

    [FEATURE] #22 Home Activity UI

--- feature-home/src/main/java/com/eshc/feature/home/HomeActivity.kt
+++ feature-home/src/main/java/com/eshc/feature/home/HomeActivity.kt
@@ -1,12 +1,26 @@
 package com.eshc.feature.home
 
-
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.eshc.feature.home.databinding.ActivityHomeBinding
+import com.google.android.material.tabs.TabLayout
 
 class HomeActivity : AppCompatActivity() {
+    private var _binding : ActivityHomeBinding? = null
+    private val binding get() = _binding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        _binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_home
+        )
+
+        binding?.lifecycleOwner = this
+
     }
 }

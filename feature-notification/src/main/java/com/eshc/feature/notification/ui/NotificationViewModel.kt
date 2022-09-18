06bc06a3b6eae627f8b41c56913907b6bedb443f
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-18 12:01:17 +0900

    [FEATURE] #12 Notification Fragment UI

---
+++ feature-notification/src/main/java/com/eshc/feature/notification/ui/NotificationViewModel.kt
@@ -0,0 +1,8 @@
+package com.eshc.feature.notification.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class NotificationViewModel : ViewModel() {
+}

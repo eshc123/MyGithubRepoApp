a11b6b9822b565f1080b79c11bcea0c529bdd4f7
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-19 12:28:41 +0900

    [FEATURE] #3 Github_auth Url 수정

--- feature-login/src/main/java/com/eshc/feature/login/common/Constant.kt
+++ feature-login/src/main/java/com/eshc/feature/login/common/Constant.kt
@@ -2,4 +2,4 @@ package com.eshc.feature.login.common
 
 import com.eshc.feature.login.BuildConfig
 
-const val GITHUB_AUTH = "https://github.com/login/oauth/authorize?client_id=${BuildConfig.githubClientId}&scope=user%20repo?"
+const val GITHUB_AUTH = "https://github.com/login/oauth/authorize?client_id=${BuildConfig.githubClientId}&scope=user+repo"

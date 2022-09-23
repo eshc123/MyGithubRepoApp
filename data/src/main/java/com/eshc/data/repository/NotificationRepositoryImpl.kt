2c7d92b7002f88dd7d3a26cf3a8d62c56a02daf5
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-23 22:32:58 +0900

    [FEATURE] #13 Notification Repository 수정

--- data/src/main/java/com/eshc/data/repository/NotificationRepositoryImpl.kt
+++ data/src/main/java/com/eshc/data/repository/NotificationRepositoryImpl.kt
@@ -1,12 +1,29 @@
 package com.eshc.data.repository
 
+import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.rxjava3.flowable
+import com.eshc.data.source.remote.NotificationPagingSource
 import com.eshc.domain.model.Notification
 import com.eshc.domain.repository.NotificationRepository
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
 
-class NotificationRepositoryImpl : NotificationRepository {
-    override fun getNotifications(): Single<Result<List<Notification>>> {
-        TODO("Not yet implemented")
+class NotificationRepositoryImpl @Inject constructor(
+    private val notificationPagingSource: NotificationPagingSource
+) : NotificationRepository {
+    override fun getNotifications(): Flowable<PagingData<Notification>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 10
+            ),
+            pagingSourceFactory = {
+                notificationPagingSource
+            }
+        ).flowable
     }
 
     override fun updateNotificationAsRead() {

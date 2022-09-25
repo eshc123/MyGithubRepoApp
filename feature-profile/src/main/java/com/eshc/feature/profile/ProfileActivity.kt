01d6176bf3fc21a2d4afc27122ae4de017af837e
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-25 16:34:50 +0900

    [FEATURE] #16 profile Activity 추가

---
+++ feature-profile/src/main/java/com/eshc/feature/profile/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.eshc.feature.profile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}

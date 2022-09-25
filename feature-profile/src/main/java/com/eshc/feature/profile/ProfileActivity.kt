e935651098e4af59e05812578461fb6fd9cf1da9
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-25 16:58:29 +0900

    [FEATURE] #16 profile Activity UI 구현

--- feature-profile/src/main/java/com/eshc/feature/profile/ProfileActivity.kt
+++ feature-profile/src/main/java/com/eshc/feature/profile/ProfileActivity.kt
@@ -1,11 +1,22 @@
 package com.eshc.feature.profile
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.eshc.feature.profile.databinding.ActivityProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileActivity : AppCompatActivity() {
+    private var _binding : ActivityProfileBinding? = null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        _binding = DataBindingUtil.setContentView(
+            this,R.layout.activity_profile
+        )
+        binding?.lifecycleOwner = this
+
     }
 }

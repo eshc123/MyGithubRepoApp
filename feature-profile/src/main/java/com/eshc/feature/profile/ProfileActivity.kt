02f3a69f99288339c76f768ddf5a6931e4cd81da
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-29 08:43:21 +0900

    [FEATURE] #30 Profile Ui 연동

--- feature-profile/src/main/java/com/eshc/feature/profile/ProfileActivity.kt
+++ feature-profile/src/main/java/com/eshc/feature/profile/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.eshc.feature.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.eshc.feature.profile.databinding.ActivityProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,12 +12,15 @@ class ProfileActivity : AppCompatActivity() {
     private var _binding : ActivityProfileBinding? = null
     private val binding get() = _binding
 
+    private val viewModel : ProfileViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(
             this,R.layout.activity_profile
         )
         binding?.lifecycleOwner = this
-
+        binding?.viewModel = viewModel
+        viewModel.getUser()
     }
 }

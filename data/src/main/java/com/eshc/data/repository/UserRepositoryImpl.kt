eeeb3757c227b7e189859a827f3e2310d6178352
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-28 17:02:30 +0900

    [FEATURE] #22 UserDataSource,UserRepository getUser 구현

--- data/src/main/java/com/eshc/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/eshc/data/repository/UserRepositoryImpl.kt
@@ -1,11 +1,27 @@
 package com.eshc.data.repository
 
+import com.eshc.data.source.UserDataSource
 import com.eshc.domain.model.User
 import com.eshc.domain.repository.UserRepository
 import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
 
-class UserRepositoryImpl : UserRepository{
+class UserRepositoryImpl @Inject constructor(
+    private val userDataSource: UserDataSource
+) : UserRepository{
     override fun getUser(): Single<Result<User>> {
-        TODO("Not yet implemented")
+        return try {
+            userDataSource.getUser()
+                .map {
+                    Result.success(it.getOrThrow())
+                }
+                .onErrorReturn {
+                    Result.failure(it.cause ?: Throwable())
+                }
+        } catch (e : Exception) {
+            Single.create {
+                Result.failure<String>(e)
+            }
+        }
     }
 }

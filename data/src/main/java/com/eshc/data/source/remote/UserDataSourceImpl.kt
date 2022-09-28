68bb945f185830a01da2e6ff368dcdd6dcd32280
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-28 20:21:24 +0900

    [REFACTOR] default User 수정

--- data/src/main/java/com/eshc/data/source/remote/UserDataSourceImpl.kt
+++ data/src/main/java/com/eshc/data/source/remote/UserDataSourceImpl.kt
@@ -4,7 +4,6 @@ import com.eshc.data.model.toUser
 import com.eshc.data.source.UserDataSource
 import com.eshc.data.source.remote.api.GithubService
 import com.eshc.domain.model.User
-import com.eshc.domain.model.defaultUser
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
@@ -14,7 +13,7 @@ class UserDataSourceImpl @Inject constructor(
     override fun getUser(): Single<Result<User>> {
         return githubService.getUserData()
             .map {
-                Result.success(it.body()?.toUser() ?: defaultUser())
+                Result.success(it.body()?.toUser() ?: User())
             }
             .onErrorReturn {
                 Result.failure(it.cause ?: Throwable("User Error"))
--- domain/src/main/java/com/eshc/domain/model/User.kt
+++ domain/src/main/java/com/eshc/domain/model/User.kt
@@ -1,31 +1,16 @@
 package com.eshc.domain.model
 
 data class User(
-    val id : Long,
-    val login : String,
-    val avatarUrl : String,
-    val name : String,
-    val blog : String,
-    val location : String,
-    val email : String,
-    val bio : String,
-    val publicRepos : Int,
-    val followers : Int,
-    val following : Int
+    val id : Long = -1,
+    val login : String = "",
+    val avatarUrl : String = "",
+    val name : String = "",
+    val blog : String = "",
+    val location : String = "",
+    val email : String = "",
+    val bio : String = "",
+    val publicRepos : Int = 0,
+    val followers : Int = 0,
+    val following : Int = 0
 )
 
-fun defaultUser() : User {
-    return User(
-        id = -1,
-        login = "",
-        avatarUrl = "",
-        name = "",
-        blog = "",
-        location = "",
-        email = "",
-        bio = "",
-        publicRepos = 0,
-        followers = 0,
-        following = 0
-    )
-}

da6711aded2869994ef875dbbfb227c6f96cd577
Jung SeokJoon <eshc.joon@gmail.com>
2022-10-10 22:37:31 +0900

    [FEATURE] #11 IssueDataSource 수정 및 IssuePagingSource cnrk

--- data/src/main/java/com/eshc/data/source/IssueDataSource.kt
+++ data/src/main/java/com/eshc/data/source/IssueDataSource.kt
@@ -1,8 +1,11 @@
 package com.eshc.data.source
 
+import androidx.paging.PagingData
 import com.eshc.domain.model.Issue
+import com.eshc.domain.model.IssueState
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
 
 interface IssueDataSource {
-    fun getIssues(state : String) : Single<Result<List<Issue>>>
+    fun getIssues(state : IssueState) : Flowable<PagingData<Issue>>
 }

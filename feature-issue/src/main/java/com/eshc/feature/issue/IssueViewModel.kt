ec9ce1ca98b55773e766dce61b6be43e291e6f4b
Jung SeokJoon <eshc.joon@gmail.com>
2022-10-12 22:35:19 +0900

    [FEATURE] #11 IssueViewModel 및 IssueFragment 데이터 연동

--- feature-issue/src/main/java/com/eshc/feature/issue/IssueViewModel.kt
+++ feature-issue/src/main/java/com/eshc/feature/issue/IssueViewModel.kt
@@ -3,16 +3,25 @@ package com.eshc.feature.issue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.map
+import androidx.paging.rxjava3.cachedIn
+import com.eshc.domain.model.IssueState
+import com.eshc.domain.usecase.issue.GetIssuesUseCase
 import com.eshc.feature.issue.model.IssueModel
+import com.eshc.feature.issue.model.toIssueModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
 @HiltViewModel
 class IssueViewModel @Inject constructor(
-
+    private val getIssuesUseCase: GetIssuesUseCase
 ) : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
@@ -22,6 +31,25 @@ class IssueViewModel @Inject constructor(
 
     private fun addDisposable(disposable: Disposable) {
         compositeDisposable.add(disposable)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun getIssues(){
+        addDisposable(
+            getIssuesUseCase(state = IssueState.all)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map {
+                    it.map { issue ->
+                        issue.toIssueModel()
+                    }
+                }.cachedIn(
+                    viewModelScope
+                )
+                .subscribe {
+                    _issues.value = it
+                }
+        )
     }
 
     override fun onCleared() {

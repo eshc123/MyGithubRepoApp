18c2f03612457efe1b5e4376900a3657804d9fb9
Jung SeokJoon <eshc.joon@gmail.com>
2022-10-15 21:21:35 +0900

    [FEATURE] #11 Issue Filter - DropDownItem onItemSelectedListener 추가 및 구현

--- feature-issue/src/main/java/com/eshc/feature/issue/ui/adapter/FilterSpinnerAdapter.kt
+++ feature-issue/src/main/java/com/eshc/feature/issue/ui/adapter/FilterSpinnerAdapter.kt
@@ -6,30 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.eshc.domain.model.IssueState
 import com.eshc.feature.issue.R
 import com.eshc.feature.issue.databinding.ItemSpinnerBinding
 import com.eshc.feature.issue.databinding.ItemSpinnerHeaderBinding
-import com.eshc.feature.issue.model.IssueOptionModel
 
 class FilterSpinnerAdapter(
     context : Context,
-    private val items : List<IssueOptionModel>
-) : ArrayAdapter<IssueOptionModel>(context, R.layout.item_spinner) {
+    private val items : List<IssueState>
+) : ArrayAdapter<IssueState>(context, R.layout.item_spinner) {
+
+    private var selectedItem : IssueState = IssueState.open
 
     override fun getCount() = items.size
 
     override fun getItem(position: Int) = items[position]
 
+    fun selectItem(state : IssueState) {
+        selectedItem = state
+    }
+
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val binding = ItemSpinnerHeaderBinding.inflate(LayoutInflater.from(parent.context),parent, false )
-        binding.issueOption = getItem(position)
+        binding.issueState = getItem(position)
         return binding.root
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         val binding = ItemSpinnerBinding.inflate(LayoutInflater.from(parent.context),parent, false )
-        binding.issueOption = getItem(position)
+        binding.issueState = getItem(position)
+        binding.ivCheck.visibility = if(selectedItem == getItem(position)) View.VISIBLE else View.INVISIBLE
         return binding.root
     }
 

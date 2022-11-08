2b9675b05ca927425799d8a5ea7ffa40849dee91
Jung SeokJoon <eshc.joon@gmail.com>
2022-11-08 22:34:29 +0900

    [FEATURE] #14 Repository ui - initViews

--- feature-repository/src/main/java/com/eshc/feature/repository/ui/RepositoryActivity.kt
+++ feature-repository/src/main/java/com/eshc/feature/repository/ui/RepositoryActivity.kt
@@ -1,17 +1,29 @@
 package com.eshc.feature.repository.ui
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.eshc.feature.repository.R
 import com.eshc.feature.repository.databinding.ActivityRepositoryBinding
+import com.eshc.feature.repository.ui.adapter.RepositoryAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.core.Observable
 
+@AndroidEntryPoint
 class RepositoryActivity : AppCompatActivity() {
     private var _binding : ActivityRepositoryBinding? = null
     private val binding get() = _binding
 
     private val viewModel : RepositoryViewModel by viewModels()
+
+    private val repoAdapter : RepositoryAdapter by lazy {
+        RepositoryAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +33,30 @@ class RepositoryActivity : AppCompatActivity() {
         binding?.lifecycleOwner = this
         binding?.viewModel = viewModel
 
+        binding?.let {
+            initRecyclerView(it.rvRepository)
+            initEditText(it.etSearch)
+        }
+
+        initObserver()
+    }
+
+    private fun initRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = repoAdapter
+    }
+
+    private fun initObserver() {
+        viewModel.repos.observe(this) {
+            repoAdapter.submitData(lifecycle,it)
+        }
+
+    }
+
+    private fun initEditText(editText: EditText) {
+        editText.addTextChangedListener {
+            viewModel.setSearchWord(it.toString())
+        }
     }
 }

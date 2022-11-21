f41cc19a76588701d269f7e2ea3149d905191ddd
Jung SeokJoon <eshc.joon@gmail.com>
2022-11-21 15:04:48 +0900

    [REFACTOR] Data,Domain - getIssues() Coroutine, Flow 사용하여 변경

--- data/src/main/java/com/eshc/data/repository/IssueRepositoryImpl.kt
+++ data/src/main/java/com/eshc/data/repository/IssueRepositoryImpl.kt
@@ -5,14 +5,13 @@ import com.eshc.data.source.IssueDataSource
 import com.eshc.domain.model.Issue
 import com.eshc.domain.model.IssueState
 import com.eshc.domain.repository.IssueRepository
-import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class IssueRepositoryImpl @Inject constructor(
     private val issueDataSource: IssueDataSource
 ) : IssueRepository {
-    override fun getIssues(state: IssueState): Flowable<PagingData<Issue>> {
+    override fun getIssues(state: IssueState): Flow<PagingData<Issue>> {
         return issueDataSource.getIssues(state)
     }
 }
--- domain/src/main/java/com/eshc/domain/usecase/issue/GetIssuesUseCase.kt
+++ domain/src/main/java/com/eshc/domain/usecase/issue/GetIssuesUseCase.kt
@@ -4,13 +4,13 @@ import androidx.paging.PagingData
 import com.eshc.domain.model.Issue
 import com.eshc.domain.model.IssueState
 import com.eshc.domain.repository.IssueRepository
-import io.reactivex.rxjava3.core.Flowable
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetIssuesUseCase @Inject constructor(
     private val issueRepository: IssueRepository
 ){
-    operator fun invoke(state : IssueState) : Flowable<PagingData<Issue>> {
+    operator fun invoke(state : IssueState) : Flow<PagingData<Issue>> {
         return issueRepository.getIssues(state)
     }
 }

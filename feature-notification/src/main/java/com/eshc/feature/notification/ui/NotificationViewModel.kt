3bbb11e0f7f92fe25309944c82b2b145c5a8ebbd
Jung SeokJoon <eshc.joon@gmail.com>
2022-11-30 12:07:30 +0900

    [REFACTOR] NotificationViewModel - removeNotifications coroutine 사용하여 구현

--- feature-notification/src/main/java/com/eshc/feature/notification/ui/NotificationViewModel.kt
+++ feature-notification/src/main/java/com/eshc/feature/notification/ui/NotificationViewModel.kt
@@ -1,25 +1,20 @@
 package com.eshc.feature.notification.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.filter
 import androidx.paging.map
-import androidx.paging.rxjava3.cachedIn
 import com.eshc.domain.usecase.notification.GetNotificationsUseCase
 import com.eshc.domain.usecase.notification.UpdateNotificationsAsReadUseCase
 import com.eshc.feature.notification.model.NotificationModel
 import com.eshc.feature.notification.model.toNotificationModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -28,31 +23,46 @@ class NotificationViewModel @Inject constructor(
     private val updateNotificationsAsReadUseCase: UpdateNotificationsAsReadUseCase
 ) : ViewModel() {
 
-    val notifications = getNotificationsUseCase().map { it.map { it.toNotificationModel() } }.cachedIn(viewModelScope)
+    private val _notificationsToBeRemoved = MutableStateFlow<List<NotificationModel>>(
+        mutableListOf()
+    )
+    val notificationsToBeRemoved: StateFlow<List<NotificationModel>> get() = _notificationsToBeRemoved
+    val isEmptyNotificationsToBeRemoved: Boolean
+        get() = notificationsToBeRemoved.value.isEmpty()
 
-    private val notificationsToBeRemoved : MutableList<NotificationModel> = mutableListOf()
-    val isEmptyNotificationsToBeRemoved : Boolean
-        get() = notificationsToBeRemoved.isEmpty()
+    val notifications =
+        notificationsToBeRemoved.flatMapLatest { removeList ->
+            getNotificationsUseCase()
+                .map {
+                    it.map {
+                        it.toNotificationModel()
+                    }
+                    .filter {
+                        removeList.map {
+                            it.id
+                        }.contains(it.id).not()
+                    }
+                }.cachedIn(viewModelScope)
+        }
 
+    val hasRemoved = MutableStateFlow<Boolean>(false)
 
+    fun removeNotification(notification: NotificationModel) {
+        _notificationsToBeRemoved.value = notificationsToBeRemoved.value + notification
+    }
 
-//    fun removeNotification(notification: NotificationModel) {
-//        _notifications.value = _notifications.value?.filter {
-//            it.id != notification.id
-//        }
-//        notificationsToBeRemoved.add(notification)
-//    }
-//
-//    fun removeAllNotifications() {
-//        if(isEmptyNotificationsToBeRemoved.not()) {
-//            updateNotificationsAsReadUseCase(
-//                notificationsToBeRemoved.map {
-//                    it.id
-//                }
-//            )
-//            notificationsToBeRemoved.clear()
-//            getNotifications()
-//        }
-//    }
+    fun removeAllNotifications() {
+        if(isEmptyNotificationsToBeRemoved.not()) {
+            viewModelScope.launch {
+                updateNotificationsAsReadUseCase(
+                    notificationsToBeRemoved.value.map {
+                        it.id
+                    }
+                )
+                _notificationsToBeRemoved.value = listOf()
+                hasRemoved.value = true
+            }
+        }
+    }
 
 }
